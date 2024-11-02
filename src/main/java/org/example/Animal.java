f6200b201260f7dc797c517e53ba9bacad11780b
Mohammed N <mnaser11218@gmail.com>
2024-11-02 14:46:33 -0400

    added food method in animal interface

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,14 +1,19 @@
 package org.example;
 
 public abstract class Animal implements AnimalInterface {
+    private final String food;
     private String sound;
 
-    public Animal(String sound) {
+    public Animal(String sound, String food) {
         this.sound = sound;
+        this.food = food;
     }
 
 public String getSound(){
         return sound;
+}
+public String getFood(){
+        return food;
 }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void printSpeech(Animal... animals){
         for(Animal ele: animals){
         ele.speak();
+        ele.eatFood();
         }
     }
 
